8eca709e9a92b7fdc5f7b5bf940cbe5f0ed88097
gaoxiafei <18335598934@163.com>
2020-05-19 17:26:54 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,91 @@
+from flask import Flask, jsonify, render_template, request
+from tools.db_manage import db_connect,db_insert,db_update,db_delete,db_select
+from tools.time_conversion import date_to_timestamp,timestamp_to_date
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def tem():
+    return render_template('tem.html')
+@app.route('/add_mobile')
+def add_tem():
+    return  render_template('add_tem.html')
+@app.route('/update_mobile')
+def update_tem():
+    return  render_template('update_mobile.html')
+
+# 查询数据
+@app.route('/select',methods=['post'])
+def select_mobile():
+    mobile={
+        'id':'',
+        'state':''
+    }
+    mobile.update({'id':request.form.get('id')})
+    mobile.update({'state':request.form.get('state')})
+    rows=db_select('mobile_manage',**mobile)
+    print(rows)
+    for i in rows:
+        # print(i.get('createdAt'))
+        i.update({'createdAt':str(i.get('createdAt'))[:-7],'updatedAt':str(i.get('updatedAt'))[:-7]})
+        print(i)
+    print(rows)
+    return jsonify({'code':0,'result':rows})
+
+#新增数据
+@app.route('/add',methods=['post'])
+def addMobile():
+    mobile={
+        'mobile_model':'',
+        'mobile_name':'',
+        'mobile_color':'',
+        'mobile_price':'',
+        'owner':'',
+        'state':''
+    }
+    mobile.update({'mobile_model':request.form.get('model')})
+    mobile.update({'mobile_name':request.form.get('name')})
+    mobile.update({'mobile_color':request.form.get('color')})
+    mobile.update({'mobile_price':request.form.get('price')})
+    mobile.update({'owner':request.form.get('owner')})
+    mobile.update({'state':1})
+    db_insert('mobile_manage',**mobile)
+    return '添加成功'
+
+
+#删除数据
+@app.route('/delete',methods=['post'])
+def deleteMobile():
+    mobile={
+        'id':''
+    }
+    mobile.update({'id':request.form.get('id')})
+    db_delete('mobile_manage',**mobile)
+    return '成功'
+
+#编辑数据
+@app.route('/update',methods=['post'])
+def updateMobile():
+    id=request.form.get('id')
+    print(id)
+    mobile={
+        'mobile_model':'',
+        'mobile_name':'',
+        'mobile_color':'',
+        'mobile_price':'',
+        'owner':''
+    }
+    mobile.update({'mobile_model':request.form.get('model')})
+    mobile.update({'mobile_name':request.form.get('name')})
+    mobile.update({'mobile_color':request.form.get('color')})
+    mobile.update({'mobile_price':request.form.get('price')})
+    mobile.update({'owner':request.form.get('owner')})
+    db_update('mobile_manage',id,**mobile)
+    return '成功'
+
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ tools/db_manage.py
@@ -0,0 +1,118 @@
+import psycopg2
+from psycopg2.extras import DictCursor
+import psycopg2.extras
+from datetime import datetime
+
+
+def db_time():
+    localtime = datetime.now()
+    return localtime
+
+
+def db_connect():
+    conn = psycopg2.connect(database='mobilemanage-test', host='10.8.8.123', user='postgres', password='1to6pg10',
+                            port=5432)
+    return conn
+
+
+# 新增数据sql
+def db_insert(table, **dict):
+    sql = '''INSERT INTO {} (*) VALUES (*) '''
+    time = db_time()
+    dict.update({'"createdAt"': time})
+    dict.update({'"updatedAt"': time})
+    for (k, v) in dict.items():
+        a = sql.split('*')
+        a.insert(1, k)
+        a.insert(2, ',*')
+        a.insert(4, '\'' + str(v) + '\'')
+        a.insert(5, ',*')
+        str_tmp = ''
+        for i in a:
+            str_tmp += str(i)
+        sql = str_tmp
+        print(sql)
+    sql = sql.format(table).replace(',*', '')
+    conn = db_connect()
+    cur = conn.cursor()
+    cur.execute(sql)
+    rowcount = cur.rowcount
+    print('新增受影响行数：', rowcount)
+    conn.commit()
+    conn.close()
+
+
+# 删除数据sql
+def db_delete(table, **dict):
+    # sql = '''delete from {} where id={}'''
+    sql = '''update {} set {} where id={}'''
+    state = "{}={}".format('state', 0)
+    for value in dict.values():
+        print(value)
+        sql = sql.format(table, state, value)
+        print(sql)
+    conn = db_connect()
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
+
+
+# 编辑数据sql
+def db_update(table, condition, **dict):
+    time = db_time()
+    dict.update({'"updatedAt"': time})
+    sql = '''update {} set {} where id={}'''
+    # 新建更新列表
+    kvlist = []
+    for k in dict:
+        kvlist.append("{}={}".format(k, '\'' + str(dict[k]) + '\''))
+    # 更新列表转化为字符串
+    kvlist = ','.join(kvlist)
+    sql = sql.format(table, kvlist, condition)
+    # 连接数据库
+    conn = db_connect()
+    # 创建游标
+    cur = conn.cursor()
+    # 执行sql
+    cur.execute(sql)
+    # 受影响行数
+    rowcount = cur.rowcount
+    print('编辑受影响行数：', rowcount)
+    # 提交
+    conn.commit()
+    # 关闭
+    conn.close()
+
+
+
+# 查询数据sql
+def db_select(table, **dict):
+    sql = '''select * from {} where {}={} '''
+    if (dict['state']!=None):
+        sql = sql.format(table, 'state', dict['state'])
+    else:
+        sql = sql.format(table, 'id', dict['id'])
+    conn = db_connect()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute(sql)
+    result = cur.fetchall()
+    return result
+
+
+class DB_Connect():
+    def __init__(self):
+        self.conn = psycopg2.connect(database='mobilemanage-test', host='10.8.8.123', user='postgres',
+                                     password='1to6pg10',
+                                     port=5432)
+
+    def getDB(self):
+        return self.conn
+
+
+if __name__ == '__main__':
+    # db_insert('mobile_manage', mobile_model='xiaomni', mobile_color=78, mobile_name='xiafei',owner='xiafei')
+    # db_delete('mobile_manage',id=13)
+    db_select('mobile_manage')
+    # db_update('mobile_manage',13,mobile_model='bianji', mobile_color='red', mobile_name='vivo',owner='xiafeigao')
+    # db_time()
